f42b4a2d994ee0953a221193c45de2a1d64a54ab
sogurtsov <sogurtsov@rightandabove.com>
2019-08-15 14:40:12 +0300

    extended plugin API to start chat

--- lib/zendesk_flutter_plugin.dart
+++ lib/zendesk_flutter_plugin.dart
@@ -3,11 +3,14 @@ import 'dart:async';
 import 'package:flutter/services.dart';
 
 class ZendeskFlutterPlugin {
-  static const MethodChannel _channel =
-      const MethodChannel('zendesk_flutter_plugin');
+  static const MethodChannel _channel = const MethodChannel('zendesk_flutter_plugin');
 
   static Future<String> get platformVersion async {
     final String version = await _channel.invokeMethod('getPlatformVersion');
     return version;
+  }
+
+  static Future<void> startChat() async {
+    await _channel.invokeMethod('startChat');
   }
 }
