6224a1424651b115ee8e8b07399c7ed948705b10
Gabriel Ferraz <gabriel@getchange.com>
2019-08-19 10:44:37 +0300

    Fixing the result call type and the parameters for the chat initialization

--- ios/Classes/ZendeskFlutterPlugin.m
+++ ios/Classes/ZendeskFlutterPlugin.m
@@ -14,18 +14,26 @@
 - (void)handleMethodCall:(FlutterMethodCall*)call result:(FlutterResult)result {
   if ([@"init" isEqualToString:call.method]) {
     [ZDCChat initializeWithAccountKey:call.arguments[@"accountKey"]];
-    result(@(true));
+    result(nil);
   } else if ([@"getPlatformVersion" isEqualToString:call.method]) {
     result([@"iOS " stringByAppendingString:[[UIDevice currentDevice] systemVersion]]);
   } else if ([@"startChat" isEqualToString:call.method]) {
     NSString *name = call.arguments[@"visitorName"];
+    NSString *email = call.arguments[@"visitorEmail"];
+    NSString *phoneNumber = call.arguments[@"visitorPhone"];
     [ZDCChat updateVisitor:^(ZDCVisitorInfo *user) {
       if (![name isKindOfClass:[NSNull class]]) {
               user.name = name;
           }
+      if (![email isKindOfClass:[NSNull class]]) {
+              user.email = email;
+          }
+      if (![phoneNumber isKindOfClass:[NSNull class]]) {
+              user.phone = phoneNumber;
+          }
     }];
     [ZDCChat startChat:nil];
-    result(@(true));
+    result(nil);
   } else {
     result(FlutterMethodNotImplemented);
   }
