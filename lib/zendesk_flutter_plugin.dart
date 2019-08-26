549753d62ea067cd04610349918272efaea506c2
Gabriel Ferraz <gabriel@getchange.com>
2019-08-26 13:03:47 +0300

    Added integration for request list and request ticket for android

--- lib/zendesk_flutter_plugin.dart
+++ lib/zendesk_flutter_plugin.dart
@@ -27,6 +27,25 @@ class ZendeskFlutterPlugin {
     });
   }
 
+  static Future<void> initSupport(String zendeskUrl, String appId, String clientId) async {
+    await _channel.invokeMethod('initSupport', <String, dynamic> {
+      'zendeskUrl': zendeskUrl,
+      'appId': appId,
+      'clientId': clientId
+    });
+  }
+
+  static Future<void> startRequestSupport() async {
+    await _channel.invokeMethod('startRequestSupport', <String, dynamic> {
+    });
+  }
+
+  static Future<void> startListRequestSupport() async {
+    await _channel.invokeMethod('startListRequestSupport', <String, dynamic> {
+
+    });
+  }
+
   static Future<void> startChat({String visitorName, String visitorEmail, String visitorPhone}) async {
     await _channel.invokeMethod('startChat', <String, dynamic>{
       'visitorName': visitorName,
