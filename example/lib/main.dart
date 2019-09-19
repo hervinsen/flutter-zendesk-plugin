93ea849f5bb52eb6ab6926d7ff2effaa0fd6a303
sogurtsov <sogurtsov@rightandabove.com>
2019-09-19 18:17:27 +0300

    fixed typo

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -16,7 +16,7 @@ class MyApp extends StatefulWidget {
 class _MyAppState extends State<MyApp> {
   String _platformVersion = 'Unknown';
   String _chatStatus = 'Uninitialized';
-  String _zendeskAccountkey = '2a4ijImfXbSkhZAUaUowwsKJZ7248PpL';
+  String _zendeskAccountkey = '';
 
   final ZendeskFlutterPlugin _chatApi = ZendeskFlutterPlugin();
 
